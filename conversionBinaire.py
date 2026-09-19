adf618ad65d858b4055711d7f647ec1e78d07d5d
IanisDum <atrid.cornot@gmail.com>
2026-09-20 00:08:11 +0200

    ajout du choix des bases, traitement de la base selectionner et creation de la fonction pour convertir de base 10 en base 2 et correction de bug liee a celle-ci

--- conversionBinaire.py
+++ conversionBinaire.py
@@ -1,15 +1,31 @@
+choix = int(input("choisis une base 2, 10, 16 : "))
 
-base = input("choisis un nombre base 10 : ")
-reste = 0
-quotient = None
+def B10xB2():
+    base = int(input("choisis un nombre base 10 : "))
+    quotient = base
+    
+    while quotient != 0:
+        
+        quotient = int(base) // 2
+        reste = int(base) % 2
+        base = quotient
+        print(reste)
 
-while quotient != 0:
-    quotient = int(base) // 2
-    reste = int(base) % 2
-    base = quotient
-    print(reste)
 
 
-print("fin")
+if choix in (2, 10, 16):
+    if choix == 10:
+        B10xB2()
+    elif choix == 2:
+        print("a suivre")
+    else:
+        print("a suivre")
 
-#fais par ianis (marche nickel)
+
+    print("fin")
+else:
+    print("erreur base non disponible")
+    exit
+
+
+    
